c1269b03b666c4b9130be47c29539018ae84368d
dewslyse <dewslyse@gmail.com>
2022-06-01 19:56:42 +0000

    Link animals table to owners table

--- schema.sql
+++ schema.sql
@@ -34,3 +34,7 @@ DROP COLUMN species;
 -- Add column species_id which is a foreign key referencing species table
 ALTER TABLE animals
 ADD COLUMN species_id INT REFERENCES species (id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners (id);
