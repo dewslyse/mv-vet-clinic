e0022708390a2a284ef51587100dd0f25daadc22
dewslyse <dewslyse@gmail.com>
2022-06-02 20:17:59 +0000

    Populate specializations table with data

--- data.sql
+++ data.sql
@@ -79,3 +79,11 @@ VALUES
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-04'),
 ('Jack Harkness', 38, '2008-06-08');
+
+-- Insert data for specialties
+INSERT INTO specializations (vets_id, species_id)
+VALUES
+(1, 1),
+(3, 1),
+(3, 2),
+(4, 2);
