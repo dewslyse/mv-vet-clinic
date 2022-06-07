b54745eb91eab57981440b0338ae0a0bd1935851
dewslyse <dewslyse@gmail.com>
2022-06-07 19:25:15 +0000

    Create medical history table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,5 +5,14 @@ CREATE TABLE patients (
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255),
   date_of_birth DATE,
-  PRIMARY KEY(id),
+  PRIMARY KEY(id)
+);
+
+-- Medical history table
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP(0),
+  patient_id INT,
+  status VARCHAR(255),
+  PRIMARY KEY(id)
 );
